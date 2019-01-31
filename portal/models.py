0bb4915e4d891724dd5e8cfc3871998db3b6a329
Daniel Miranda <developermiranda@gmail.com>
2019-01-31 17:29:54 -0200

    Criando Model Category

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+  slug = models.SlugField(unique=True)
+  parent = models.ForeignKey('Category', null=True, blank=True, related_name='cat_child', on_delete=models.CASCADE)
+  order = models.IntegerField(null=True, blank=True)
+  hidden = models.BooleanField(default=False)
+
